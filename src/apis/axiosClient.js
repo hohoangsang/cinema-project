c50f8705492dc9c79c0053a434d86a616fbd07df
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2022-10-07 22:25:49 +0700

    update base url api

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import queryString from 'query-string'
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://server-cgv-cinema.herokuapp.com",
     headers: {
         'content-type': 'application/json'
     },
